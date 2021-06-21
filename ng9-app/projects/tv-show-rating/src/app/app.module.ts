e557dcdae858920d985c16e15c7713f191e7ccbd
Diego <diegotgjeptha@gmail.com>
2021-06-21 19:43:16 +0200

    Adding a Form to Rate TV Shows

--- ng9-app/projects/tv-show-rating/src/app/app.module.ts
+++ ng9-app/projects/tv-show-rating/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TvRatingFormComponent } from './tv-rating-form/tv-rating-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TvRatingFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
